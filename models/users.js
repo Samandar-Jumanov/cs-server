3f7dd3a7d0b93597521485b3c5e2240be26c8077
Samandar-Jumanov <samandarjumanovudemy@gmail.com>
2023-09-10 02:18:45 +0500

    fixing bugs with db

--- models/users.js
+++ models/users.js
@@ -1,1 +1,45 @@
-c
+const {DataTypes } = require('sequelize')
+const sequelize = require('../utils/db')
+const { SentSolutions, RecievedSolutions  } = require('./solutions')
+const SharedProblems = require('./sharedProblems')
+
+
+const Users = sequelize.define('users', {
+    id : {
+        type : DataTypes.INTEGER,
+        autoIncrement : true ,
+        primaryKey : true 
+    },
+    username : {
+        type : DataTypes.STRING ,
+        allowNull : false ,
+        unique : true 
+    },
+    password : {
+        type : DataTypes.STRING ,
+        allowNull : false 
+    }, 
+    token :{
+       type: DataTypes.STRING ,
+       allowNull : false 
+    }
+})
+Users.hasMany(SharedProblems , {
+    as :'sharedProblems'
+})
+
+//Sent solutions and users 
+Users.hasMany(SentSolutions , {
+     as :'sentSolutions'
+})
+
+//Recieved Solutions 
+
+Users.hasMany(RecievedSolutions,{
+     as :'recievedSolutions'
+})
+
+
+
+
+module.exports = Users
