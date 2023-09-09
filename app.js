c1ecea03dfcbc76c6bb9d7bc189c6d12c6947d17
Samandar-Jumanov <samandarjumanovudemy@gmail.com>
2023-09-09 11:08:14 +0500

    added cors

--- app.js
+++ app.js
@@ -3,10 +3,20 @@ const sequelize = require('./utils/db')
 const {usersRouter} = require('./routes/users')
 const { problemRouter } = require('./routes/problems')
 const solutionRouter = require('./routes/solutions')
+const cors = requrie('cors')
 const app = express()
 
 
 app.use(express.json())
+
+// Enable CORS with specific options
+app.use(
+    cors({
+      origin: '*', 
+      methods: ['GET', 'POST' , 'PUT', 'DELETE']  , 
+      allowedHeaders: ['Content-Type', 'Authorization'] // Allow only specified headers
+    })
+  );
 
 app.get('/', (req, res , next )=>{
     res.send('Started')
