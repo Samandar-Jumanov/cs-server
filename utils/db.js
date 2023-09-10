bb98ca082d084cb489e6fa321819ba450ce24ac1
Samandar-Jumanov <samandarjumanovudemy@gmail.com>
2023-09-10 09:42:07 +0500

    working with db

--- utils/db.js
+++ utils/db.js
@@ -1,7 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('ppostgres://samandarjumanov:R89pzY1ZLn7HFuuVWZ9lxnoXW1vadoDw@dpg-cjqsc38jbais738niub0-a/db_code_sender', {
-  host: 'localhost',
+const sequelize = new Sequelize('postgres://samandarjumanov:R89pzY1ZLn7HFuuVWZ9lxnoXW1vadoDw@dpg-cjqsc38jbais738niub0-a/db_code_sender', {
   dialect: 'postgres',
 });
 
