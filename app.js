775dfbdea860c0ab881c0d897ab2cb73535f178d
Samandar-Jumanov <samandarjumanovudemy@gmail.com>
2023-09-13 17:07:40 +0500

    changing models

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const sequelize = require('./utils/db')
 const {usersRouter} = require('./routes/users')
 const { problemRouter } = require('./routes/problems')
 const solutionRouter = require('./routes/solutions')
@@ -26,11 +25,7 @@ app.use('/api/v1/users', usersRouter)
 app.use('/api/v1/problems', problemRouter)
 app.use('/api/v1/solutions', solutionRouter)
 
-sequelize.sync().then(()=>{
-    console.log('Connected db ')
-}).catch((err)=>{
-    console.log(err)
-})
+
 
 app.listen(3001 ,()=>{
   console.log('Server started ')
