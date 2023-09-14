68aacd55c2ea09efaff2949153e8b2dd0b59e8b4
Samandar-Jumanov <samandarjumanovudemy@gmail.com>
2023-09-14 09:47:09 +0500

    fziign bugs

--- routes/shareProblems.js
+++ routes/shareProblems.js
@@ -2,5 +2,7 @@ const problemsRouter = require('express').Router()
 const shareProblemsController = require('../controller/shareProblems')
 
 problemsRouter.post('/create', shareProblemsController.shareProblem)
+problemsRouter.get('/all', shareProblemsController.getAllProblems)
+
 
 module.exports = problemsRouter
