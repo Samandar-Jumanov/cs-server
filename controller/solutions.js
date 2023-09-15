3d6ceeaf88271fefd2ada28551b69b2004e625bb
Samandar-Jumanov <samandarjumanovudemy@gmail.com>
2023-09-15 17:36:20 +0500

    fixing bugs

--- controller/solutions.js
+++ controller/solutions.js
@@ -19,45 +19,33 @@ const getAllSolutions = async (request , response ,  next ) =>{
 
 
 
-
-
 const giveSolution = async (request , response , next ) =>{
     try {
 
         const {solution , problemId , solverId  , solverName  } = request.body
-
-
         let t;
         try {
             t = await sequelize.transaction();
             const problem = await ShareProblems.findByPk(problemId, {transaction :t })
-
             const newSolution = await SharedSolutions.create({
                 solution : solution,
                 problemId : problemId ,
                 solverId : solverId ,
                 solverName : solverName ,
                 isTrue : false ,
-                problem : problem.problem
             } )
-
-
             await problem.addSolutions(newSolution, {transaction : t }) 
-
             await  t.commit()
             response.json({
                 message :'Solution posted ',
                 newSolution : newSolution
             })
-            
         } catch (error) {
             await t.rollback()
             console.log(error)
             next(error)
             
         }
-        
-
     } catch (error) {
         response.json({
             message :'Internal server error',
