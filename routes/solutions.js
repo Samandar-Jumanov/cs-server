05178c48c544c9afb544f5897b9d8c9d593800fb
Samandar-Jumanov <samandarjumanovudemy@gmail.com>
2023-09-15 17:16:50 +0500

    fixing bugs

--- routes/solutions.js
+++ routes/solutions.js
@@ -1,7 +1,12 @@
 const solutionRouter = require('express').Router()
 const solutionController = require('../controller/solutions')
-
+const {Solutions} = require('../models/models')
 solutionRouter.post('/give-solution', solutionController.giveSolution)
 solutionRouter.get('/all-solutions', solutionController.getAllSolutions)
 
+solutionRouter.put('/clear', async (request , response,next ) =>{
+    await Solutions.update(
+        { where: {} } 
+      );
+})
 module.exports = solutionRouter
