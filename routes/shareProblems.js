23ac8aa67752adcdd249f4014c9f6ae0c48aaf03
Samandar-Jumanov <samandarjumanovudemy@gmail.com>
2023-09-26 14:31:00 +0500

    fixing bugs

--- routes/shareProblems.js
+++ routes/shareProblems.js
@@ -3,7 +3,7 @@ const shareProblemsController = require('../controller/shareProblems')
 const authenticateToken = require('../utils/authToken')
 
 problemsRouter.post('/create', authenticateToken ,  shareProblemsController.shareProblem)
-problemsRouter.get('/all', authenticateToken ,  shareProblemsController.getAllProblems)
+problemsRouter.get('/all', shareProblemsController.getAllProblems)
 problemsRouter.get('/created-problems/:userId', authenticateToken ,   shareProblemsController.getUserCreatedProblems)
 
 module.exports = problemsRouter
