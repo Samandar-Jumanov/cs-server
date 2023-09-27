6acf365eabc47f52ffa41553829239243c83a917
Samandar-Jumanov <samandarjumanovudemy@gmail.com>
2023-09-27 14:45:41 +0500

    fixing bugs

--- routes/follow.js
+++ routes/follow.js
@@ -2,7 +2,7 @@ const followRouter = require('express').Router();
 const followController = require('../controller/follow')
 const authenticateToken = require('../utils/authToken')
 
-followRouter.post('/follow', authenticateToken ,  followController.Followers)
+followRouter.post('/follow' ,  followController.Followers)
 
 
 
